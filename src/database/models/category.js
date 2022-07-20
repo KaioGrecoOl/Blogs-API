0e6d71446a0b7165c068e52098abed070774aece
Kaio G <kaiogreco91@gmail.com>
2022-07-20 17:36:16 -0300

    feat(model)cria o modelo Category

---
+++ src/database/models/category.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//JSdocs
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {  
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  }, {
+    tableName: 'Categories',
+    timestamps: false
+  })
+  return Category;
+};
+
+module.exports = Category;
