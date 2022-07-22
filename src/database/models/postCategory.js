cf13b4c94a9d40a1555868c755f2fc18436a2ee7
Kaio G <kaiogreco91@gmail.com>
2022-07-22 17:58:08 -0300

    feat(model)cria o modelo postcategory com as devidas propriedades e associações

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//JSdocs
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  }, {
+    tableName: 'PostCategories',
+  });
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost,
+      { through: 'PostCategory', foreignKey: 'postId', as: 'BlogPosts', otherKey: 'categoryId' });
+  
+    models.BlogPost.belongsToMany(models.Category,
+      { through: 'PostCategory', foreignKey: 'categoryId', as: 'Categories', otherKey: 'postId' });
+    };
+  return PostCategory;
+};
+
+module.exports = PostCategory;
